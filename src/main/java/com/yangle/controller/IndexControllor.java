3ac87a6cdccf3b29363d74612292e07668fa7fff
yangle <884454075@qq.com>
2017-12-08 15:33:14 +0800

    代码格式化

--- src/main/java/com/yangle/controller/IndexControllor.java
+++ src/main/java/com/yangle/controller/IndexControllor.java
@@ -28,98 +28,105 @@ import java.util.UUID;
  */
 @Controller
 public class IndexControllor {
-    private static final java.lang.String ROOT ="D:\\blog-files\\";
+    private static final java.lang.String ROOT = "D:\\blog-files\\";
     private final ResourceLoader resourceLoader;
 
     @Autowired
     public IndexControllor(ResourceLoader resourceLoader) {
         this.resourceLoader = resourceLoader;
     }
+
     @RequestMapping("/")
-    public String index(){
+    public String index() {
         return "index";
     }
 
     @RequestMapping("/to_login")
-    public String toLogin(){
+    public String toLogin() {
         return "login";
     }
+
     @RequestMapping("/to_write")
-public String to_write(){
+    public String to_write() {
         return "writearticle";
-}
+    }
 
-@RequestMapping("/to_manage_article")
-public String to_manage_article(){
+    @RequestMapping("/to_manage_article")
+    public String to_manage_article() {
         return "glwz";
-}
-@RequestMapping("/to_timezhou")
-public String to_timezhou(){
+    }
+
+    @RequestMapping("/to_timezhou")
+    public String to_timezhou() {
         return "timezhou";
-}
-@RequestMapping("/to_tages")
-public String to_tages(){
+    }
+
+    @RequestMapping("/to_tages")
+    public String to_tages() {
         return "tages";
-}
+    }
 
 
-
-@ResponseBody
-@RequestMapping("/uploadImage")
-public Map uploadImage(HttpServletRequest request, String img,String fileExtendName ){
-    Map map=new HashMap();
-    map.put("msg","上传成功");
-    System.out.println(img);
-    BASE64Decoder decoder = new BASE64Decoder();
-try {
-img=img.substring(img.indexOf(",")+1,img.length());
-byte[] b = decoder.decodeBuffer(img);
-
-for (int i = 0; i < b.length; ++i) {
-if (b[i] < 0) {
-b[i] += 256;
-}
-}
-
-    Files.copy(new ByteArrayInputStream(b) , Paths.get(ROOT, UUID.randomUUID().toString().replaceAll("-", "")+"."+fileExtendName));
-
-
-} catch (FileNotFoundException e) {
-        e.printStackTrace();
-    } catch (IOException e) {
-        e.printStackTrace();
-    }return map;}
-/**
- * 上传文件
- */
-@ResponseBody
-@RequestMapping(method = RequestMethod.POST, value = "/uploadFile")
-public Map handleFileUpload(@RequestParam("file") MultipartFile file,HttpServletRequest request) {
-    Map map=new HashMap();
-    if (!file.isEmpty()) {
+    @ResponseBody
+    @RequestMapping("/uploadImage")
+    public Map uploadImage(HttpServletRequest request, String img, String fileExtendName) {
+        Map map = new HashMap();
+        map.put("msg", "上传成功");
+        System.out.println(img);
+        BASE64Decoder decoder = new BASE64Decoder();
         try {
-            String name=file.getOriginalFilename();
-            String fileExtendName=name.substring(name.indexOf(".")+1,name.length());
-            Files.copy(file.getInputStream() , Paths.get(ROOT, UUID.randomUUID().toString().replaceAll("-", "")+"."+fileExtendName));
-            map.put("errno",0);
-            map.put("data",new String[]{request.getContextPath()+"/images/"+file.getOriginalFilename()});
-            return map;
-        }  catch (IOException e) {
+            img = img.substring(img.indexOf(",") + 1, img.length());
+            byte[] b = decoder.decodeBuffer(img);
+
+            for (int i = 0; i < b.length; ++i) {
+                if (b[i] < 0) {
+                    b[i] += 256;
+                }
+            }
+
+            Files.copy(new ByteArrayInputStream(b), Paths.get(ROOT, UUID.randomUUID().toString().replaceAll("-", "") + "." + fileExtendName));
+
+
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
-            map.put("errno",1);
-            map.put("data",new String[]{request.getContextPath()+"/images/"+file.getOriginalFilename()});
-            return map;
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-}  else {
-        map.put("errno",1);
-        map.put("data",new String[]{request.getContextPath()+"/images/"+file.getOriginalFilename()});
         return map;
     }
 
-}
+    /**
+     * 上传文件
+     */
+    @ResponseBody
+    @RequestMapping(method = RequestMethod.POST, value = "/uploadFile")
+    public Map handleFileUpload(@RequestParam("file") MultipartFile file, HttpServletRequest request) {
+        Map map = new HashMap();
+        if (!file.isEmpty()) {
+            try {
+                String name = file.getOriginalFilename();
+                String fileExtendName = name.substring(name.indexOf(".") + 1, name.length());
+                Files.copy(file.getInputStream(), Paths.get(ROOT, UUID.randomUUID().toString().replaceAll("-", "") + "." + fileExtendName));
+                map.put("errno", 0);
+                map.put("data", new String[]{request.getContextPath() + "/images/" + file.getOriginalFilename()});
+                return map;
+            } catch (IOException e) {
+                e.printStackTrace();
+                map.put("errno", 1);
+                map.put("data", new String[]{request.getContextPath() + "/images/" + file.getOriginalFilename()});
+                return map;
+            }
+        } else {
+            map.put("errno", 1);
+            map.put("data", new String[]{request.getContextPath() + "/images/" + file.getOriginalFilename()});
+            return map;
+        }
+
+    }
 
     /**
      * 显示图片
+     *
      * @param filename
      * @return
      */
@@ -133,6 +140,7 @@ public Map handleFileUpload(@RequestParam("file") MultipartFile file,HttpServlet
             return ResponseEntity.notFound().build();
         }
     }
+
     /**
      * 测试转pdf
      */
@@ -202,9 +210,9 @@ public Map handleFileUpload(@RequestParam("file") MultipartFile file,HttpServlet
         // PDFEncryption(null,null,PdfWriter.ALLOW_PRINTING);
         // renderer.setPDFEncryption(pdfEncryption); //只有打印权限的
         renderer.setDocument(url);
-        String aa=this.getClass().getResource("/static").getPath();
-        renderer.getFontResolver().addFont("C:/WINDOWS/Fonts/SIMSUN.TTC",BaseFont.IDENTITY_H, BaseFont.NOT_EMBEDDED);
-        renderer.getSharedContext().setBaseURL( new File(aa).toURI().toURL().toString());
+        String aa = this.getClass().getResource("/static").getPath();
+        renderer.getFontResolver().addFont("C:/WINDOWS/Fonts/SIMSUN.TTC", BaseFont.IDENTITY_H, BaseFont.NOT_EMBEDDED);
+        renderer.getSharedContext().setBaseURL(new File(aa).toURI().toURL().toString());
 
         renderer.layout();
         try {
